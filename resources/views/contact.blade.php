66cc6e300f162d397abf7cef71b4301eb6ccf901
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-03-05 16:59:10 +0700

    EDIT

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,22 +10,22 @@ License URI: http://www.themevault.net/license/-->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="images/icons/favicon.png"/>
+        <link rel="icon" href="a/images/icons/favicon.png"/>
         <title>vacayhome</title>
 
         <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="a/css/bootstrap.min.css" rel="stylesheet">
+        <link href="a/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <!-- Custom styles for this template -->
-        <link href="css/style.css" rel="stylesheet">
-        <link href="fonts/antonio-exotic/stylesheet.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/lightbox.min.css">
-        <link href="css/responsive.css" rel="stylesheet">
-        <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="js/lightbox-plus-jquery.min.js" type="text/javascript"></script>
-        <script src="js/instafeed.min.js" type="text/javascript"></script>
-        <script src="js/custom.js" type="text/javascript"></script>
+        <link href="a/css/style.css" rel="stylesheet">
+        <link href="a/fonts/antonio-exotic/stylesheet.css" rel="stylesheet">
+        <link rel="stylesheet" href="a/css/lightbox.min.css">
+        <link href="a/css/responsive.css" rel="stylesheet">
+        <script src="a/js/jquery.min.js" type="text/javascript"></script>
+        <script src="a/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="a/js/lightbox-plus-jquery.min.js" type="text/javascript"></script>
+        <script src="a/js/instafeed.min.js" type="text/javascript"></script>
+        <script src="a/js/custom.js" type="text/javascript"></script>
     </head>
     <body>
         <div id="page">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/about', function () {return view('about');})->name('about');
+Route::get('/', function () {return view('about');})->name('about');
